070417ccc7154483853f2a723f7254b0638a353a
ronchris <ponferrada.ronald@gmail.com>
2015-08-12 00:15:20 -0700

    updated slider effects

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-	// Set the interval to be 2 seconds
-	var t = setInterval (function(){
-		$("#carousel ul").animate({marginLeft:-60},1000)
-	},2000);
+// $(document).ready(function() {
+
+// 	// Set interval to 2 seconds
+// 	var t = setInterval(function(){
+// 		$("#carousel ul").animate({marginLeft:-60},"slow",function(){
+// 			$('#carousel ul').css({marginLeft:0});
+// 			setInterval();
+// 		})
+// 	},2000);
+// });
+
+$(document).ready(function() {
+  function loop() {
+    $('#carousel ul').css({marginLeft:100});
+      $('#carousel ul').animate ({
+        marginLeft: '+=-200',
+      }, 10000, 'linear', function() {
+        loop();
+    });
+  }
+  loop();
 });
+
